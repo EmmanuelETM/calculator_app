d2a50d83680a03a3a09b33dc9817f94420127994
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-12-18 22:50:00 -0400

    Add the math functions for our operations, and export them to app.js

--- src/math.js
+++ src/math.js
@@ -1,15 +1,14 @@
-const add = (x, y) => {
-    return x + y;
+const add = (x, y) => x + y;
+const substract = (x, y) =>  x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+const operator = (x, y, op) => {
+    if(op === '+') return add(x, y);
+    else if (op === '-') return substract(x, y);
+    else if (op === '*') return multiply(x, y);
+    else if (op === '/') return divide(x, y);
+    else return 'Error';
 }
 
-const substract = (x, y) => {
-    return x - y;
-}
-
-const multiply = (x, y) => {
-    return x * y;
-}
-
-const divide = (x, y) => {
-    return x / y;
-}
+export { operator };

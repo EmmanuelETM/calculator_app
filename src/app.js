5611e233f645c3f0e97ac116d02036bf05b05008
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-12-20 12:36:54 -0400

    Add a function to handle the number buttons, and working on the operator buttons

--- src/app.js
+++ src/app.js
@@ -1,59 +1,75 @@
-import { operator } from './math.js'
+import { operator } from './math.js';
+
+const calcState = {
+    num1 : '',
+    num2: '',
+    op: '',
+    result: '',
+
+}
 
 const handleClear = (state, screen) => {
-    screen.textContent = '';
+    console.log('clear')
     state.num1 = '';
     state.num2 = '';
     state.op = '';
     state.result = '';
+    screen.textContent = '';
 }
 
-const handleNumbers = (state, number) => {
-    if(state.op = ''){
-        state.num1 = number;
-    }else {
-        state.num2 = number;
+const handleNumbers = (state, num) => {
+    if(state.op === '') {
+        state.num1 = num;
+
     }
-    return state;
+    else {
+        state.num2 = num;
+    }
 }
 
-const handleOperators = (event, state) => {
-    if (state.num1 != '' && state.num2 === '') {
-        state.op = ''; //change
+//we have work to do on this bitch
+
+const handleOperators = (state, op, screen) => {
+    if (state.num1 !== '') {
+        if (state.num2 === '') {
+            state.op = op
+            screen.textContent = '';
+            state.result = '';
+        }
     }
     
-    if (state.num2 != '') {
+    if (state.num2 !== '') {
         state.result = operator(state.num1, state.num2, state.op);
+        screen.textContent = state.result
+        state.num1 = state.result;
+        state.num2 = '';
+        state.op = op;
+        state.result = '';
     }
+
+    console.log(state)
 }
 
-const handleCalcButtons = (event,) => {
+const App = (calcState) => {
+    const calcButtons = document.querySelector('.calc-buttons');
+    const screenText = document.querySelector('.screen-text');
+    const ACButton = document.querySelector('.AC');
+
+    calcButtons.addEventListener('click', (event) => {
+        if (event.target.classList.contains('num')) {
+            screenText.textContent += event.target.textContent;
+            handleNumbers(calcState, screenText.textContent)
+            console.log(calcState);
+        }
+
+        if (event.target.classList.contains('op')) {
+            handleOperators(calcState, event.target.textContent, screenText);
+        }
+    })
+
+    ACButton.addEventListener('click', () => {handleClear(calcState, screenText)});
     
 }
 
-const calcButtons = document.querySelector('.calc-buttons');
-const screenText = document.querySelector('.screen-text');
-const ACButton = document.querySelector('.AC');
-const equalButton = document.querySelector('.equal');
-const pointButton = document.querySelector('.point');
 
-const calcState = {
-    num1: '', 
-    num2: '', 
-    op: '',
-    result: ''
-}
-
-
-calcButtons.addEventListener('click', (event) => {
-
-})
-
-ACButton.addEventListener('click', handleClear(calcState, screenText));
-
-
-// pointButton.addEventListener('click', (event) => {
-//     if(!screenText.textContent.includes('.')){
-//         screenText.textContent += '.';
-//     }
-// })
+App(calcState);

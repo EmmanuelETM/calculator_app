80ad8e6adcf0b088d28a82c450314787e5043606
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-12-19 21:11:51 -0400

    Placed the buttons in correct order. Add color to calculator

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 import { operator } from './math.js'
 
+
+const calcButtons = document.querySelector('.calc-buttons');
+const screenText = document.querySelector('.screen-text');
+const ACButton = document.querySelector('.AC');
+
+
 let num1 = 10;
 let num2 = 5;
 let op = '/';
+let screenValue = '';
 
-// console.log(operator(num1, num2, op))
+
+calcButtons.addEventListener('click', (event) => {
+    if(event.target.classList.contains('num')) {
+        screenText.textContent += event.target.textContent;
+        screenValue = screenText.textContent;
+    }
+})
+
+ACButton.addEventListener('click', (event) => {
+    screenText.textContent = '';
+    screenValue = '';
+    num1 = '';
+    num2 = '';
+    op = '';
+})

1271e1c0c970308a45a3c394a46a1153d83269b5
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-12-20 21:13:29 -0400

    Restart the work on the logic

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-import { operator } from './math.js';
-
+// Global Variables
 let num1 = null;
 let num2 = null;
 let op1 = null;
@@ -7,6 +6,7 @@ let op2 = null;
 let result = null;
 let screenValue = '0';
 
+// Query selectors for the html elements
 const buttons = document.querySelector('.calc-buttons')
 const clearButton = document.querySelector('.AC');
 const equal = document.querySelector('.equal');
@@ -14,6 +14,21 @@ const point = document.querySelector('.point');
 const negative = document.querySelector('.negative');
 const percent = document.querySelector('.percent');
 
+// Math Functions
+const add = (x, y) => x + y;
+const subtract = (x, y) =>  x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => y !== 0 ? x / y : 'Wack Division';
+
+const operator = (x, y, op) => {
+    if (op === '+') return add(x, y);
+    else if (op === '-') return subtract(x, y);
+    else if (op === '*') return multiply(x, y);
+    else if (op === '/') return divide(x, y);
+    else return 'Error';
+}
+
+// Calc Functions
 const clearScreen = () => {
     num1 = null;
     num2 = null;
